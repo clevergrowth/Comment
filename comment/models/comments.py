afab99864eadf0c7f34d4c807f6abf68a238f918
Ankit kumar <ankitkhadria1@gmail.com>
2020-10-13 15:24:35 +0530

    change object type integer to char

--- comment/models/comments.py
+++ comment/models/comments.py
@@ -16,7 +16,7 @@ class Comment(models.Model):
     email = models.EmailField(blank=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
+    object_id = models.CharField(max_length=256, null=False, blank=False, default=None)
     content_object = GenericForeignKey('content_type', 'object_id')
     content = models.TextField()
     urlhash = models.CharField(
